89d1a2677a63739d78277c18cca4b38e7f8c1b12
oscar <ocl5@alu.ua.es>
2021-10-19 20:17:43 +0200

    update and delete method implemented, not comment owner in updategit add .git add .!

--- controllers/comment.js
+++ controllers/comment.js
@@ -45,7 +45,6 @@ var controller = {
                 content : params.content
             }
             
-
             issetTopic.comments.push(newComment);
 
             //Save comment
@@ -62,18 +61,89 @@ var controller = {
                 }
             });           
         });
-
     }, //close save controller
 
     update : function(req, res){
-        return res.status(200).send({
-            msg : "Update de comment controller"
+        var params = req.body;
+
+        //Validate information
+        try{
+            //Validate data
+            var validate_content = !validator.isEmpty(params.content);
+        }catch(e){
+            return res.status(400).send({
+                msg: "Too few information."
+            });
+        }
+
+        //GUARD CLAUSE
+        if(!validate_content ){
+            return res.status(400).send({
+                msg: "Form's data isn't valid."
+            });
+        } 
+
+        Topic.findOneAndUpdate({"comments._id" : req.params.idComment}, 
+                                {"$set" : {"comments.$.content" : params.content}},   
+                                {new:true},                                 
+                                (err, updatedTopic) => {
+            
+            if(err){
+                return res.status(500).send({
+                    msg: "Error. Couldnt connect to DB." + err.message
+                });
+            }
+
+            if(!updatedTopic){
+                return res.status(404).send({
+                    msg: "Error. Comment doesnt exist, then you cant update it."
+                });
+            }    
+
+            return res.status(200).send({
+                msg: "Comment updated successfully.",
+            });
+
         });
     }, //close update controller
 
     delete : function(req, res){
-        return res.status(200).send({
-            msg : "Delete de comment controller"
+        var params = req.body;
+        
+        Topic.findById(req.params.idTopic, (err, issetTopic) => {
+            
+            if(err){
+                return res.status(500).send({
+                    msg: "Error. Couldnt connect to DB." + err.message
+                });
+            }
+
+            if(!issetTopic){
+                return res.status(404).send({
+                    msg: "Error. Topic doesnt exist."
+                });
+            }
+
+            var comment = issetTopic.comments.id(req.params.idComment);
+            
+            if(!comment){
+                return res.status(404).send({
+                    msg: "Error. Comment doesnt exist."
+                });
+            }
+
+            if(req.user.sub != comment.user){
+                return res.status(403).send({
+                    msg: "Error. You cant delete this comment."
+                });
+            }
+
+            comment.remove();
+            issetTopic.save();
+
+            return res.status(200).send({
+                msg: "Comment deleted succesfully."
+            });
         });
     } //close delete controller
 }
