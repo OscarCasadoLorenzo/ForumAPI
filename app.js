98d1fb344d7ee1a78d0df5a1e01dad2158fcd3e4
oscar <ocl5@alu.ua.es>
2021-10-19 20:29:09 +0200

    CORS configurated

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ var comment_routes = require('./routes/comment');
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-//CORS
+/* 
+    When we make AYAX request with jQuery or Angular
+    to an API REST frecuently we will have problems
+    with the CORS. 
+    To solve it, we can add this middleware
+*/
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //Rewrite routes
 app.use('/whyme', user_routes);
