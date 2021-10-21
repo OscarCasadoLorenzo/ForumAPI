c1c8289ea4ebe74c017ab3645471d5c7f104621d
oscar <ocl5@alu.ua.es>
2021-10-21 10:50:08 +0200

    various files renamed and autodoc with swagger

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var express = require('express');
 var logger = require('morgan');
 var fs = require('fs');
 
+var swaggerUI = require('swagger-ui-express');
+var swaggerFile = require('./others/swagger_doc.json');
+
 //Run express
 var app = express();
 
@@ -16,9 +19,9 @@ var comment_routes = require('./routes/comment');
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(logger('common', {
-    stream: fs.createWriteStream('./debugFiles/access.log', {flags: 'a'})
+    stream: fs.createWriteStream('./others/access.log', {flags: 'a'})
 }));
-
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 /* 
     When we make AYAX request with jQuery or Angular
     to an API REST frecuently we will have problems
